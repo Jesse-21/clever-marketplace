879e509782dfbd730ab5d97bd311cfd861759c58
suiramdev <mariusnouchet@gmail.com>
2021-12-16 21:55:30 +0100

    Moved from api to getStaticProps WIP API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,35 @@
-import type {NextPage} from 'next';
+import type {GetStaticProps, NextPage} from 'next';
 import styles from "../styles/Explore.module.scss";
+import {ListingMetadata, ThirdwebSDK} from "@3rdweb/sdk";
+import {ethers} from "ethers";
+import Config from "../config.json";
 import Card from "../components/Card";
 
-const Explore: NextPage = () => {
+interface IExploreProps {
+    nfts: any
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const sdk = new ThirdwebSDK(
+        new ethers.Wallet(
+            process.env.PRIVATE_KEY as string,
+            ethers.getDefaultProvider(Config.thirdweb.provider)
+        )
+    );
+    const market = sdk.getMarketModule(Config.thirdweb.module);
+    console.log(await market.getAll());
+    return {
+        props: {
+            nfts: "d"
+        }
+    }
+}
+
+const Explore: NextPage<IExploreProps> = ({nfts}) => {
+    console.log(nfts);
     return (
         <div className={styles.explore}>
-            <Card/>
-            <Card/>
+
         </div>
     )
 }
