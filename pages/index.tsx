ba74b1e1d3dfdd796f265c7f12f2fa3cb600a774
suiramdev <mariusnouchet@gmail.com>
2021-12-16 15:52:08 +0100

    Removed thirdweb SDK interaction on front-end as it is moved to API/ on next commits

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,8 @@
 import type {NextPage} from 'next';
 import styles from "../styles/Explore.module.scss";
 import Card from "../components/Card";
-import {ThirdwebSDK} from "@3rdweb/sdk";
-import {ethers} from "ethers";
-import Config from "../config.json";
 
 const Explore: NextPage = () => {
-    const sdk = new ThirdwebSDK(
-        new ethers.Wallet(
-            Config.wallet.privateKey,
-            ethers.getDefaultProvider(Config.thirdweb.provider)
-        )
-    );
-    const market = sdk.getMarketModule(Config.thirdweb.module);
-    console.log(market.getAll());
-
     return (
         <div className={styles.explore}>
             <Card/>
